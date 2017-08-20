5a9fdc7ffe88654661ff37a9afb613efc1a1dab4
ben hooper <benhooper13@gmail.com>
2017-08-19 22:36:34 -0400

    initial checkin

---
+++ mysite/polls/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("hello World")
+
+def detail(request, question_id):
+    return HttpResponse("you're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "your'e looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("you're voting on question %s." % question_id)
+
